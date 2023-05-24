49ec7bf86e9c73e966451419c262440f3be895f1
balan <balanovkirill@gmail.com>
2023-05-24 12:13:32 +0300

    Color set to delete button

--- app/src/main/java/com/example/easyshop/AddDialog.kt
+++ app/src/main/java/com/example/easyshop/AddDialog.kt
@@ -6,36 +6,35 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.example.easyshop.domain.Product
 
-class EditDialog(
+class AddDialog(
     context: Context,
-    private val product: Product,
-    private val onEdit: (Product) -> Unit,
-    private val onDelete: (Product) -> Unit
+    private val onAdd: (Product) -> Unit, //fixme
 ) : AlertDialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTitle("Edit")
+        setTitle("Add")
         setMessage("Enter new value:")
         val input = EditText(context)
-        input.setText(product.name)
+        input.setText("Add item")
         setView(input)
 
         setButton(BUTTON_POSITIVE, "Ok") { dialog, which ->
-            val newValue = input.text.toString()
-            val product1 = Product(product.id, newValue, product.checked)
-            onEdit.invoke(product1)
+
+//            val newValue = input.text.toString()
+//            val product1 = Product(product.id, newValue, product.checked)
+//            onEdit.invoke(product1)
         }
 
         setButton(BUTTON_NEGATIVE, "Cancel") { dialog, which ->
             dialog.cancel()
         }
 
-        setButton(BUTTON_NEUTRAL, "Delete") { dialog, which ->
-            onDelete.invoke(product)
-        }
+//        setButton(BUTTON_NEUTRAL, "Delete") { dialog, which ->
+//            onDelete.invoke(product)
+//        }
 
         super.onCreate(savedInstanceState)
 
-        getButton(BUTTON_NEUTRAL).setTextColor(context.getColor(R.color.red))
+//        getButton(BUTTON_NEUTRAL).setTextColor(context.getColor(R.color.red))
     }
 }
